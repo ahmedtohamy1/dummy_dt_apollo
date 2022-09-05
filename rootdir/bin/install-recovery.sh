c817138c672c045aa89dc32f480eba52319bc76a
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2022-09-05 17:55:58 +0000

    global .6

--- rootdir/bin/install-recovery.sh
+++ rootdir/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/vendor/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:27e92241004ff4b2ebdc9ae4fc6ca539eed19cdc; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:7832ab05cf7d6d3e601a03dc4f57ed9b255c0514; then
   applypatch  \
           --patch /vendor/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:1ee565f80d361414a1a36e20a9d8bfa6f95a84cc \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:27e92241004ff4b2ebdc9ae4fc6ca539eed19cdc && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot$(getprop ro.boot.slot_suffix):134217728:bcab7c48b3cf522ea1140dc5a2e19933d71eea61 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery$(getprop ro.boot.slot_suffix):134217728:7832ab05cf7d6d3e601a03dc4f57ed9b255c0514 && \
       log -t recovery "Installing new recovery image: succeeded" || \
       log -t recovery "Installing new recovery image: failed"
 else
